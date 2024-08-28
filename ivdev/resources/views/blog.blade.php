a4b0d0a5a609e871f363f732d33510025a0c20da
IrvanV <irvanvebrinew@gmail.com>
2024-08-28 19:36:54 +0700

    memahami struktur folder

---
+++ ivdev/resources/views/blog.blade.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>halaman blog</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+    <a href="/home">Home</a>
+    <a href="/about">About</a>
+    <a href="/blog">Blog</a>
+    <a href="/contact">Contact</a>
+
+    <h1>Lorem ipsum dolor sit amet.</h1>
+    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. 
+        Nostrum, velit possimus, unde maxime molestiae nam eius harum tempora, 
+        veniam explicabo eligendi omnis blanditiis qui itaque. 
+        Placeat eligendi pariatur maiores veniam, 
+        doloribus ipsa omnis dolorem labore dolorum 
+        hic ad necessitatibus distinctio similique totam velit iste? 
+        Quam, vitae, odio natus quas reiciendis aspernatur non dicta 
+        commodi distinctio architecto ipsam necessitatibus blanditiis 
+        eos quod culpa dolore consectetur, quasi molestiae mollitia cum! 
+        Aut quis eum in maiores, alias cupiditate omnis iusto neque, 
+        vel eos atque saepe aliquid dolore quisquam, 
+        distinctio quae debitis amet maxime rem enim nam doloremque! 
+        Odio provident voluptas qui cum non.</p>
+</body>
+</html>
