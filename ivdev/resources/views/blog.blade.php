702814f81f02e4ad748e25cf347e28cbbadd0b9f
IrvanV <irvanvebrinew@gmail.com>
2024-08-30 16:24:33 +0700

    Menambahkan view data pada artikel di halaman blog

--- ivdev/resources/views/blog.blade.php
+++
@@ -1,5 +0,0 @@
-<x-layout>
-    <x-slot:title>{{ $title }}</x-slot:title>
-    <h3 class="text-xl">Ini adalah halaman Blog IVDEV</h3>
-    <p>Irvan Vebriansyah Developer</p>
-  </x-layout>
--- ivdev/routes/web.php
+++ ivdev/routes/web.php
@@ -1,10 +1,38 @@
 <?php
 
+use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\Route;
 
 // Route::get('/', function () {
 //     return view('welcome');
 // });
+
+class Post 
+{
+    public static function all()
+    {
+        return [
+            [
+                'id' => 1,
+                'slug' => 'judul-artikel-1',
+                'title' =>  'Judul Artikel 1',
+                'author' => 'Irvan Vebriansyah',
+                'body' => 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. 
+                Tenetur consectetur odio magni? Quos a quae, minus deleniti laboriosam voluptates amet? 
+                Praesentium totam impedit similique beatae.'
+            ],
+            [
+                'id' => 2,
+                'slug' => 'belajar-laravel',
+                'title' =>  'Belajar Laravel',
+                'author' => 'Irvan Vebriansyah',
+                'body' => 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. 
+                Facere sunt eligendi at ratione fuga suscipit dignissimos officiis repellat maxime libero, 
+                non tempora veritatis similique quos!'
+            ]
+            ];
+    }
+};
 
 Route::get('/home', function () {
     return view('home', ['title' => 'Home Page']);
@@ -15,8 +43,17 @@ Route::get('/about', function () {
 });
 
 
-Route::get('/blog', function () {
-    return view('blog', ['title' => 'Blog']);
+Route::get('/posts', function () {
+    return view('posts', ['title' => 'Blog', 'posts' => Post::all()]);
+});
+
+Route::get('/posts/{slug}', function($slug){
+
+    $post = Arr::first(Post::all(), function($post) use ($slug) {
+        return $post['slug'] == $slug;
+    });
+
+    return view('post', ['title' => 'Single Post', 'post' => $post]);
 });
 
 Route::get('/contact', function () {
